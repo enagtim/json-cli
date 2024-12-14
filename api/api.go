8c605a4b19a0d206b4e15f531d425fd5b90e7beb
Nikita <enagtim@mail.ru>
2024-12-14 10:19:10 +0300

    Рефакторинг api

--- api/api.go
+++ api/api.go
@@ -1,11 +1,1 @@
 package api
-
-type API struct {
-	Key string
-}
-
-func NewAPI(key string) *API {
-	return &API{
-		Key: key,
-	}
-}
--- config/config.go
+++ config/config.go
@@ -12,12 +12,10 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Ошибка загрузки .env файла")
 	}
-
 	apiKey := os.Getenv("KEY")
 	if apiKey == "" {
 		log.Fatal("Переменная окружения KEY не установлена")
